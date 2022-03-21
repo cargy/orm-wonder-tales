6ce904d8329660e66c93e5ab9f9631aae100a167
argy <8470784+cargy@users.noreply.github.com>
2022-03-21 09:23:50 +0200

    bootstrap spring boot app

---
+++ src/main/java/org/wonder/tales/TalesApplication.java
@@ -0,0 +1,13 @@
+package org.wonder.tales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wonder/tales/TalesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wonder.tales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
